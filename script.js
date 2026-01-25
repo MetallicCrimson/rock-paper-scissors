4e6bf60ca340b61f39911810559800a7fbd9474d
MetallicCrimson <tamerlein667@gmail.com>
2026-01-25 22:30:10 +0100

    Write logic for single round (with output)

--- script.js
+++ script.js
@@ -21,3 +21,65 @@ function getHumanChoice() {
     
     return choice;
 }
+
+function capitalize(s) {
+    return s[0].toUpperCase() + s.slice(1);
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    // 0: win; 1: lose; 2: tie
+    let winStatus;
+
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                winStatus = 2;
+                break;
+            case "paper":
+                winStatus = 1;
+                break;
+            case "scissors":
+                winStatus = 0;
+        }
+    } else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                winStatus = 0;
+                break;
+            case "paper":
+                winStatus = 2;
+                break;
+            case "scissors":
+                winStatus = 1;
+        }
+    } else if (humanChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                winStatus = 1;
+                break;
+            case "paper":
+                winStatus = 0;
+                break;
+            case "scissors":
+                winStatus = 2;
+        }
+    }
+
+    switch (winStatus) {
+        case 0:
+            console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`);
+            humanScore++;
+            break;
+        case 1:
+            console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`);
+            computerScore++;
+            break;
+        case 2:
+            console.log(`It's a tie! Both choices were ${capitalize(humanChoice)}.`);
+            break;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
