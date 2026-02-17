c655f17e98aa03938fcf5ad06f93f2817359cb83
MetallicCrimson <tamerlein667@gmail.com>
2026-02-18 00:46:12 +0100

    Handle healthbars, tweak animations

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const SPEED = 5;
+const HEALTH_UNIT = 560 / 5;
 
 function keyDownFunction(e) {
     switch (e.code) {
@@ -58,6 +59,8 @@ const playingScreen = document.querySelector("#playing-screen");
 const shootingScreen = document.querySelector("#shooting-screen");
 const playerChoiceDiv = document.querySelector("#player-choice");
 const computerChoiceDiv = document.querySelector("#computer-choice");
+const playerHealthbar = document.querySelector("#player-healthbar");
+const computerHealthbar = document.querySelector("#computer-healthbar");
 
 document.body.addEventListener("keydown", keyDownFunction);
 document.body.addEventListener("keyup", keyUpFunction);
@@ -70,54 +73,86 @@ let downPressed = false;
 const width = 550;
 const height = 715;
 
+let playerHealth;
+let computerHealth;
+
 // let yOffset = container.getBoundingClientRect().top
 // let xOffset = container.getBoundingClientRect().left
 
-testPlayer.style.left = "265px";
-testPlayer.style.top = "650px";
 
 
+function initializeGame() {
+    playerHealth = 5;
+    computerHealth = 5;
 
-window.onload = function() {
-    
+    playerHealthbar.style.width = 5*HEALTH_UNIT + "px";
+    computerHealthbar.style.width = 5*HEALTH_UNIT + "px";
 
+    initializeRound();   
+}
 
-    function test () {
-        if (playingScreen.classList.contains("inactive")) return;
-        // testPlayer.style.top = testPlayer.getBoundingClientRect().top-40 + SPEED + "px";
-        if (leftPressed && parseInt(testPlayer.style.left) >= SPEED) {
-            testPlayer.style.left = parseInt(testPlayer.style.left) - SPEED + "px";
-            console.log(testPlayer.style.left);
-        }
-        if (rightPressed && parseInt(testPlayer.style.left) <= width - SPEED - 20) {
-            testPlayer.style.left = parseInt(testPlayer.style.left) + SPEED + "px";
-            console.log(testPlayer.style.left);
-        }
-        if (upPressed) {
-            if (parseInt(testPlayer.style.top) >= 560 - SPEED) { 
-                testPlayer.style.top = parseInt(testPlayer.style.top) - SPEED + "px";
+function initializeRound() {
+    playingScreen.classList.remove("inactive");
+    shootingScreen.classList.add("inactive");
+
+    // transition: transform 1.5s, top 1.5s;
+
+    playerChoiceDiv.style.transition = "transform 0s, top 0s";
+    computerChoiceDiv.style.transition = "transform 0s, top 0s";
+
+    playerChoiceDiv.style.transform = "scale(0)";
+    computerChoiceDiv.style.transform = "scale(0) rotate(180deg)";
+
+    setTimeout(() => {
+        playerChoiceDiv.style.transition = "transform 1.5s, top 1.5s";
+        computerChoiceDiv.style.transition = "transform 1.5s, top 1.5s";
+    });
+
+    testPlayer.style.left = "265px";
+    testPlayer.style.top = "650px";
+}
+
+function moveByFrame () {
+    if (playingScreen.classList.contains("inactive")) return;
+    // testPlayer.style.top = testPlayer.getBoundingClientRect().top-40 + SPEED + "px";
+    if (leftPressed && parseInt(testPlayer.style.left) >= SPEED) {
+        testPlayer.style.left = parseInt(testPlayer.style.left) - SPEED + "px";
+        console.log(testPlayer.style.left);
+    }
+    if (rightPressed && parseInt(testPlayer.style.left) <= width - SPEED - 20) {
+        testPlayer.style.left = parseInt(testPlayer.style.left) + SPEED + "px";
+        console.log(testPlayer.style.left);
+    }
+    if (upPressed) {
+        if (parseInt(testPlayer.style.top) >= 560 - SPEED) { 
+            testPlayer.style.top = parseInt(testPlayer.style.top) - SPEED + "px";
+        } else {
+            let playerChoice;
+            let choiceCoord = parseInt(testPlayer.style.left) + 10;
+
+            if (choiceCoord <= 180) {
+                playerChoice = 0;
+            } else if (choiceCoord <= 370) {
+                playerChoice = 1;
             } else {
-                let playerChoice;
-                let choiceCoord = parseInt(testPlayer.style.left) + 10;
-
-                if (choiceCoord <= 180) {
-                    playerChoice = 0;
-                } else if (choiceCoord <= 370) {
-                    playerChoice = 1;
-                } else {
-                    playerChoice = 2;
-                }
-
-                
-                playRound(playerChoice, Math.floor(Math.random() * 3));
+                playerChoice = 2;
             }
-        }
-        if (downPressed && parseInt(testPlayer.style.top) <= height - SPEED - 50) {
-            testPlayer.style.top = parseInt(testPlayer.style.top) + SPEED + "px";
+
+            
+            playRound(playerChoice, Math.floor(Math.random() * 3));
         }
     }
+    if (downPressed && parseInt(testPlayer.style.top) <= height - SPEED - 50) {
+        testPlayer.style.top = parseInt(testPlayer.style.top) + SPEED + "px";
+    }
+}
 
-    this.setInterval(test, (1000/60));
+window.onload = function() {
+    this.setInterval(moveByFrame, (1000/60));
+
+    initializeGame();
+
+    
 }
 
 function parseChoice(choice) {
@@ -194,11 +229,18 @@ function playRound(playerChoiceInt, computerChoiceInt) {
     computerChoiceDiv.textContent = getChoiceCharacter(computerChoiceInt);
     playerChoiceDiv.textContent = getChoiceCharacter(playerChoiceInt);
 
+    playerChoiceDiv.style.animationName = "";
+    computerChoiceDiv.style.animationName = "";
+    playerChoiceDiv.style.top = "450px";
+    playerChoiceDiv.style.left = "250px";
+    computerChoiceDiv.style.top = "250px";
+    computerChoiceDiv.style.left = "250px";
+
     computerChoiceDiv.style.transform = "scale(1) rotate(180deg)";
     playerChoiceDiv.style.transform = "scale(1)";
 
     setTimeout(() => {
-        if (true) { // (winStatus === 0) {
+        if (winStatus === 0) {
             // computerChoiceDiv.style.transition = "top .5s linear";
             // computerChoiceDiv.style.top = "330px";
             // playerChoiceDiv.style.transition = "top " + (11/4) + "s linear";
@@ -206,7 +248,37 @@ function playRound(playerChoiceInt, computerChoiceInt) {
 
             playerChoiceDiv.style.animationName = "player-winning";
             computerChoiceDiv.style.animationName = "computer-losing";
+
+            computerHealth--;
+        } else if (winStatus === 1) {
+            playerChoiceDiv.style.animationName = "player-losing";
+            computerChoiceDiv.style.animationName = "computer-winning";
+
+            playerHealth--;
+        } else {
+            playerChoiceDiv.style.animationName = "player-losing";
+            computerChoiceDiv.style.animationName = "computer-losing";
         }
+
+        setTimeout(() => {
+
+            playerHealthbar.style.width = playerHealth*HEALTH_UNIT + "px";
+            computerHealthbar.style.width = computerHealth*HEALTH_UNIT + "px";
+
+            if (playerHealth <= 0) {
+                console.log("You lose!");
+            } else if (computerHealth <= 0) {
+                console.log("You win!");
+            } else {
+                setTimeout(() => {
+                    initializeRound();
+                }, 500);
+                
+                
+                // initializeRound();
+            }
+        }, 900);
+
     }, 1500);
 
     
